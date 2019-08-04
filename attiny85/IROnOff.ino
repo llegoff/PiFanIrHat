2476b3ff00b8999cec13809d4a9cb3609064603a
Laurent Le Goff <laurent.le.goff@free.fr>
2019-08-04 15:21:09 +0200

    Add files via upload

---
+++ attiny85/IROnOff.ino
@@ -0,0 +1,53 @@
+/*
+
+ */
+
+#include <IRremote.h>
+#include <EEPROM.h>
+
+#define RECV_PIN  3
+#define RELAY_PIN  2
+#define LED_PIN  0
+#define SW_PIN  1
+
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+unsigned long good_code;
+unsigned long i;
+
+
+void setup()
+{
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SW_PIN,INPUT_PULLUP);
+  pinMode(RECV_PIN,INPUT_PULLUP);
+  irrecv.enableIRIn(); // Start the receiver
+  EEPROM.get(0, good_code); //read in eeprom
+}
+
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    if (results.value==good_code){
+        digitalWrite(RELAY_PIN,HIGH);
+        digitalWrite(LED_PIN,HIGH);
+    }
+    irrecv.resume(); // Receive the next value
+    i=50000;
+    while (i--){;}
+    digitalWrite(RELAY_PIN,LOW);
+    digitalWrite(LED_PIN,LOW);
+  }
+  // Key learning
+  if (!digitalRead(SW_PIN)){
+      digitalWrite(LED_PIN,HIGH);
+      while (!irrecv.decode(&results)) { ;}
+      EEPROM.put(0,results.value);//save in eeprom
+      irrecv.resume(); // Receive the next value
+      EEPROM.get(0, good_code); //read in eeprom
+      digitalWrite(LED_PIN,LOW);
+  } //end learning
+}
